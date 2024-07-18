a8a2e1ff74c54a2a3b29b1e7ea5dd3afe3457c80
Matthias <matthias.vimbert@gmail.com>
2024-07-18 18:25:57 +0200

    dx(app): setup prettier / linter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,24 @@
-import AuthButton from "@/components/buttons/AuthButton";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import AuthButton from '@/components/buttons/AuthButton';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>
-      <h2>My Amazing App</h2>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div>
+        <h2>My Amazing App</h2>
 
-      {session && (
-        <div>
-          <p>Signed in as {session.user && session.user.name}</p>
-          <a href="/api/auth/signout">Sign out by link</a>
-        </div>
-      )}
+        {session && (
+          <div>
+            <p>Signed in as {session.user && session.user.name}</p>
+            <a href='/api/auth/signout'>Sign out by link</a>
+          </div>
+        )}
 
-      {!session && (
-        <p>Not signed in</p>
-      )}
-
-    </div>
+        {!session && <p>Not signed in</p>}
+      </div>
       <AuthButton />
     </main>
   );
