6a322650688b7f2c565c5d817c0c7667ed359b46
Matthias <matthias.vimbert@gmail.com>
2024-08-01 21:58:56 +0200

    style(app): change shadcn theme

--- src/app/annotation/projects/page.tsx
+++ src/app/annotation/projects/page.tsx
@@ -1,28 +1,71 @@
 import { prisma } from '@/lib/prisma';
-import { AnnotationProject } from '@prisma/client';
 import ProjectItem from './components/ProjectItem';
 import Typography from '@/components/others/Typography';
 import NewProject from './components/NewProject';
+import { Suspense } from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default async function Page() {
-  const projects = await prisma.annotationProject.findMany();
-
-  return <ProjectList projects={projects} />;
-}
-
-const ProjectList = ({ projects }: { projects: AnnotationProject[] }) => {
   return (
     <div className='text-white w-full'>
       <div className='inline-block'>
-        <NewProject />
+        <h1>Hello worlds</h1>
+        <Suspense fallback={<Skeleton className='w-[300px] h-[20px] rounded-xl' />}>
+          <NewProject />
+        </Suspense>
       </div>
       <div className='pt-10'>
         <Typography variant='subheading'>Recent</Typography>
         <hr className='border-1 border-gray-500 my-2 w-full' />
-        <div className='pt-2 flex flex-wrap gap-4'>
-          {projects.map((project) => (
-            <ProjectItem key={project.id} project={project} />
-          ))}
+        <Suspense fallback={<SkeletonProjectList />}>
+          <ProjectList />
+        </Suspense>
+      </div>
+    </div>
+  );
+}
+
+const ProjectList = async () => {
+  const projects = await prisma.annotationProject.findMany();
+
+  return (
+    <div className='pt-2 flex flex-wrap gap-4'>
+      {projects.map((project) => (
+        <ProjectItem key={project.id} project={project} />
+      ))}
+    </div>
+  );
+};
+
+const SkeletonProjectList = () => {
+  return (
+    <div className='pt-2 flex flex-wrap gap-4'>
+      <div className='flex flex-col gap-2'>
+        <Skeleton className='h-[100px] w-[250px] rounded-lg' />
+        <div>
+          <Skeleton className='h-[5px] w-[100px] rounded-lg' />
+          <Skeleton className='h-[5px] w-[150px] rounded-lg' />
+        </div>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <Skeleton className='h-[100px] w-[250px] rounded-lg' />
+        <div>
+          <Skeleton className='h-[5px] w-[100px] rounded-lg' />
+          <Skeleton className='h-[5px] w-[150px] rounded-lg' />
+        </div>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <Skeleton className='h-[100px] w-[250px] rounded-lg' />
+        <div>
+          <Skeleton className='h-[5px] w-[100px] rounded-lg' />
+          <Skeleton className='h-[5px] w-[150px] rounded-lg' />
+        </div>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <Skeleton className='h-[100px] w-[250px] rounded-lg' />
+        <div>
+          <Skeleton className='h-[5px] w-[100px] rounded-lg' />
+          <Skeleton className='h-[5px] w-[150px] rounded-lg' />
         </div>
       </div>
     </div>
