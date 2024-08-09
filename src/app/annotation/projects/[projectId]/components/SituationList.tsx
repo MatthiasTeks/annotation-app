43ba2cd089caebc6f6397c3e44eb45a2caf22018
Matthias <matthias.vimbert@gmail.com>
2024-08-09 17:56:48 +0200

    feat(annotation): situation panel bases

--- src/app/annotation/projects/[projectId]/components/SituationList.tsx
+++ src/app/annotation/projects/[projectId]/components/SituationList.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { AnnotationSituation } from '@prisma/client';
+import { FileText } from 'lucide-react';
 
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function SituationList({
   situations,
@@ -11,11 +13,24 @@ export default function SituationList({
   situations: AnnotationSituation[];
   projectId: string;
 }) {
+  const pathname = usePathname();
+
+  const isActiveLink = (situation: AnnotationSituation) => {
+    return pathname === `/annotation/projects/${projectId}/situation/${situation.id}`;
+  };
+
   return (
     <div className='text-white'>
       {situations.map((situation) => (
-        <Link href={`${projectId}/situation/${situation.id}`} key={situation.id}>
-          <p key={situation.id}>{situation.name}</p>
+        <Link
+          href={`${projectId}/situation/${situation.id}`}
+          key={situation.id}
+          className={`link ${isActiveLink(situation) ? 'text-primary' : ''}`}
+        >
+          <p key={situation.id} className='text-sm flex items-center gap-2'>
+            <FileText className='mr-2 h-4 w-4' />
+            {situation.name}
+          </p>
         </Link>
       ))}
     </div>
