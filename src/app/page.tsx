156842ec2f5e19a0bcb24f2f7ee0cd297b4f16b6
Matthias <matthias.vimbert@gmail.com>
2024-08-27 23:30:40 +0200

    feat(app): redirect user on auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { auth } from '@/helpers/auth';
+import { redirect } from 'next/navigation';
+
 export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) redirect('/annotation/projects');
   return <div>You must be signed in to access to the application</div>;
 }
