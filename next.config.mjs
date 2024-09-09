a6650a15e4ec23bf6ac14d30421d8183ab4ccd49
Matthias <matthias.vimbert@gmail.com>
2024-09-09 15:13:03 +0200

    wip(upload): fix next config for remote images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'try-luminify.fr',
+        port: '',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
